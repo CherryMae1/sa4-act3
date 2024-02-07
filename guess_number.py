4d46f69ccb180b3d8101ec93609ebf125328e30c
Naimah Muhammad <naimahmuhammad07@gmail.com>
2024-02-07 16:58:17 -0600

    Added an if statement to limited the amount of guesses a user can make

--- guess_number.py
+++ guess_number.py
@@ -2,16 +2,22 @@ number = 10
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
+amt_guess = 5
 
 
 while True:
-    if guess == str(number):
-      print("Congratulations! You guessed the right number.")
+  if guess == str(number):
+    print("Congratulations! You guessed the right number.")
+    break
+  elif guess == 'q' or guess == 'Q':
+    print(f"You have quit, the number was: {number}.")
+    break
+  else:
+    amt_guess -= 1
+    print(f"You have {amt_guess} left.")
+    if amt_guess == 0:
+      print('You have run out of guesses.')
       break
-    elif guess == 'q' or guess == 'Q':
-      print(f"You have quit, the number was: {number}.")
-      break
-    else:
-      print("Please try guessing a number again or press 'q' to quit.")
-      guess = input("What number am I thinking of? ")
+    guess = input("What number am I thinking of? ")
+  
 
